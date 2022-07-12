95e8e1d27e75dcee09691428a03aff53da5c72d8
ErickS0to <erick.soto@dimex.mx>
2022-07-12 17:43:32 -0500

    prueba

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: "https://apimocha.com/dmxangular/"
 };
 
 /*
